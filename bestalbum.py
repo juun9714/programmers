a0e244051adfe852baa529a8383e5852e5bc124e
juun9714 <lovekwon200@gmail.com>
2021-12-29 15:54:39 +0900

    best album done

--- bestalbum.py
+++ bestalbum.py
@@ -2,16 +2,19 @@ g=["classic", "pop", "classic", "classic", "pop"]
 p=[500, 600, 150, 800, 2500]
 
 def solution(genres, plays):
-    tot={}
-    for i in range(len(genres)):
-        if genres[i] not in tot:
-            tot[genres[i]]=plays[i]
-        else:
-            tot[genres[i]]+=plays[i]
-
-    print(max(tot.values()))
-
     answer = []
+    gen_tot={}
+    d={}
+
+    for i in range(len(genres)):
+        gen_tot[genres[i]]=gen_tot.get(genres[i],0)+plays[i]
+        d[genres[i]]=d.get(genres[i],[])+[(plays[i],i)]
+
+    gen_sort=sorted(gen_tot.items(), key=lambda x:x[1], reverse=True)
+    for (genre, cnt) in gen_sort:
+        d[genre]=sorted(d[genre], key=lambda x:-x[0])
+        answer+=[idx for (play, idx) in d[genre][:2]]
+
     return answer
 
 solution(g,p)
