eb0b56b5c905e9415845164be5e2d2fe1205d30e
juun9714 <lovekwon200@gmail.com>
2021-12-29 16:12:23 +0900

    index order added

--- bestalbum.py
+++ bestalbum.py
@@ -12,9 +12,10 @@ def solution(genres, plays):
 
     gen_sort=sorted(gen_tot.items(), key=lambda x:x[1], reverse=True)
     for (genre, cnt) in gen_sort:
-        d[genre]=sorted(d[genre], key=lambda x:-x[0])
+        d[genre]=sorted(d[genre], key=lambda x:(-x[0], x[1]))
         answer+=[idx for (play, idx) in d[genre][:2]]
+        #list slice index를 2로 설정해도 개수가 1밖에 없으면 1개만 가져오고 만다. 
 
     return answer
 
-solution(g,p)
+print(solution(g,p))
